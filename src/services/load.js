5d3317c8588895a8907b6160171d9d05101a0af1
MGP-28 <joaosilvapereira96@hotmail.com>
2022-06-22 18:31:08 +0100

    victory lap

--- src/services/load.js
+++ src/services/load.js
@@ -2,8 +2,9 @@ import { getProducts } from './getProducts.js'
 import { addProducts } from '../store/products.js'
 
 export async function load(){
+    //get products from API
     const productsAPI = await getProducts()
+    //insert them into store array
     addProducts(productsAPI)
-    //NOT WORKING
 }
 
