76a3135712038b2f097eed9bbba860f777b646f3
MGP-28 <joaosilvapereira96@hotmail.com>
2022-06-24 10:28:40 +0100

    Product API fetch request improved

--- src/services/getProducts.js
+++ src/services/getProducts.js
@@ -1,28 +1,24 @@
 async function getProducts() {
 
   const url = 'https://fakestoreapi.com/products/category/jewelery';
-  let data
 
   try {
 
-    data = await fetch(url, {
+    const data = await fetch(url, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
       },
     })
     .then(res => res.json())
+    
+    return data
       
   } catch (error) {
 
     console.log(error, 'Error fetching data');
 
-  } finally {
-    
-    return data
-
-  }
-
+  } 
 }
 
 export { getProducts };
