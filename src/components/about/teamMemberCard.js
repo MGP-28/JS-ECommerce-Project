4f06d7fd6a041b6770b1efcf1bfe69d01d36c815
Marcio Miguel <mscanja@gmail.com>
2022-06-30 23:23:26 +0100

    small

--- src/components/about/teamMemberCard.js
+++ src/components/about/teamMemberCard.js
@@ -1,6 +1,6 @@
 function render(teamMember){
     const card = document.createElement('div')
-    card.classList.add('flex', 'flex-col', 'items-center', 'gap-5', 'w-max', 'p-5', 'border', 'shadow-lg')
+    card.classList.add('flex', 'flex-col', 'items-center', 'gap-5', 'w-max', 'p-5', 'border', 'shadow-lg', 'bg-white')
     card.innerHTML = `
         <img class="w-64 border border-gray-200 bg-gray-100" src="${teamMember.image}" alt="">
         <h4 class="text-center text-2xl">${teamMember.name}</h4>
