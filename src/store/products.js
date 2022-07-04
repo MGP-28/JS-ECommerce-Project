0fa78775ab0be7b9ef06b6150f4fef5b63cbe1c7
MGP-28 <joaosilvapereira96@hotmail.com>
2022-07-04 16:13:26 +0100

    debug cleanup

--- src/store/products.js
+++ src/store/products.js
@@ -53,12 +53,10 @@ export function removeUnitFromCart(product, removeAll = false){
     if (removeAll) { 
         product.deleteFromCart()
         cartItemsIds = cartItemsIds.filter(el => el != product.id)
-        console.log(cartItemsIds, products)
         return
     }
     if (product.quantity > 1) {
         product.removeUnitFromCart()
-        console.log(cartItemsIds, products)
         return true
     }
 }
