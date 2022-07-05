4cb9d271a9e39c71c636d9a464c60bb3c8889532
MGP-28 <joaosilvapereira96@hotmail.com>
2022-07-05 18:51:55 +0100

    Popup elements - wip

---
+++ src/components/generic/popup.js
@@ -0,0 +1,12 @@
+export function renderPopup(isConfirmation, message){
+    const container = document.createElement('div')
+    container.classList.add('fixed','w-screen','h-screen','background-half','grid','place-items-center')
+
+    const popup = document.createElement('div')
+    container.append(popup)
+    container.classList.add('background-beige','border','rounded','grid','place-items-center','w-20','h-20')
+
+    //
+
+    
+}
