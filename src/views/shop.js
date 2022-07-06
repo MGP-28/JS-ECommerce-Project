9e0d11c1c63bcb5a0a88acec0431643177c809ac
MGP-28 <joaosilvapereira96@hotmail.com>
2022-07-06 17:19:40 +0100

    Shop data loading animation

--- src/views/shop.js
+++ src/views/shop.js
@@ -8,16 +8,22 @@ export function shop(){
 
     shopContainerEl.classList.add('sm:px-16', 'py-10', 'flex', 'flex-col', 'gap-10');
 
-    document.querySelector('#shop').append(shopContainerEl);
+    const shop = document.querySelector('#shop')
+    shop.append(shopContainerEl);
     
     shopContainerEl.append(renderTopRow());
 
-    shopContainerEl.append(renderSpinner());
+    const spinner = renderSpinner()
+    spinner.classList.add('h-20')
+    shopContainerEl.append(spinner);
 
     //shopContainerEl.append()
 
     document.addEventListener('productsLoaded', (e) => {
-        //remove spinner
+        //remove spinner content
+        const spinnerEls = shopContainerEl.querySelectorAll(':scope>:not(div)') //look into the first layer into shop container and get anything that's not a Div (svg, i, ...)
+        spinnerEls.forEach(element => element.remove())
+        //render product list
         shopContainerEl.append(renderProductList());
     })
     
