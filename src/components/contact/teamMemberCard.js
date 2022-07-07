e95e4f1f66fefd772afb46f21ffc94f3998f946d
MGP-28 <joaosilvapereira96@hotmail.com>
2022-07-07 13:55:36 +0100

    Contact cards now consume API

--- src/components/contact/teamMemberCard.js
+++ src/components/contact/teamMemberCard.js
@@ -1,20 +1,17 @@
 export function teamMemberCard(teamMember){
 
-    console.log(teamMember)
-
-    /*`
-    <div class="flex flex-col items-center gap-3" id="contact-userData">
-        <img src="" alt="" id="contact-user-img">
-        <span id="contact-userName">
-        Nome do Mans
-        </span>
-        <span id="contact-userWork">
-        Trabalho do Mans
-        </span>
+    const containerEl = document.createElement('div')
+    containerEl.classList.add('flex','flex-col','items-center','gap-3')
+    containerEl.setAttribute('id', 'contact-userData')
+    containerEl.innerHTML = `
+        <img src="${teamMember.image}" alt="" id="contact-user-img">
+        <span id="contact-userName">${teamMember.name}</span>
+        <span id="contact-userWork">${teamMember.job}</span>
         <span id="contact-userDescription" class="pb-3 text-justify">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto iste necessitatibus quis culpa esse vel, labore molestiae enim. Facilis minima quas sint, dolore necessitatibus ea inventore molestias amet iste itaque.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto iste necessitatibus quis culpa esse vel, labore molestiae enim. Facilis minima quas sint, dolore necessitatibus ea inventore molestias amet iste itaque.
         </span>
-        <button type="submit" id="contact-userButton">Send Message</button>
-    </div>
-    `*/
+        <button type="submit" id="contact-userButton" class="font-parisienne border border-black hover:border-orange-300 hover:shadow-md">Send Message</button>
+    `
+
+    return containerEl
 }
--- src/services/getUsers.js
+++ src/services/getUsers.js
@@ -1,17 +1,37 @@
-import { getRandomUser } from "./getUser"
-
 async function getContactUsers(){
-    const contacts = []
 
-    for (let index = 0; index < 2; index++) {
-        const user = await getRandomUser()
-        if(user) contacts.push(user)
-        else return false
-    }
+    let retries = 1
 
-    console.log('contacts loaded')
+    while(retries > 0){
 
-    return contacts
+        try{
+            let promises = ['https://randomuser.me/api/','https://randomuser.me/api/']
+            let teamMembers = []
+            const jobs = ['Sales Representative', 'Head of Human Resources']
+
+            const responses = await Promise.all(promises.map(url => fetch(url)))
+            const data = await Promise.all(responses.map(response => response.json()))
+
+            const jobsLength = jobs.length
+            for (let index = 0; index < jobsLength; index++) {
+
+                const name = data[index].results[0].name.first + ' ' + data[index].results[0].name.last
+                const image = data[index].results[0].picture.large
+                const job = jobs[index]
+
+                const teamMember = {name: name, image: image, job: job}
+                teamMembers.push(teamMember)
+            }
+
+            return teamMembers
+
+        }catch(error){
+            retries--;
+            console.log(error)
+            console.log('contacts: retries left: ', retries)
+        }     
+    }  
+    return false
 }
 
 export {getContactUsers}
